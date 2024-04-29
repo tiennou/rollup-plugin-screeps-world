fdfe993c5dd68bb6d659c3bda9ddba650cf59eed
Erik Burton <erikbburton@gmail.com>
2024-04-28 22:49:40 -0500

    Update all deps and get prepublishOnly working

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,20 @@
 import clear from "rollup-plugin-clear";
 import ts from "rollup-plugin-typescript2";
 
-const pkg = require("./package.json");
+import pkg from "./package.json" assert { type: "json" };
 
 export default {
-	input: "./src/index.ts",
+  input: "./src/index.ts",
   output: [
     {
       file: pkg.main,
-      format: 'cjs'
+      format: "cjs",
     },
     {
       file: pkg.module,
-      format: 'es'
-    }
+      format: "es",
+    },
   ],
-	plugins: [
-    clear({"targets":["dist/"]}),
-		ts()
-	],
-  external: [ "screeps-api", "git-rev-sync", "fs", "path" ]
+  plugins: [clear({ targets: ["dist/"] }), ts()],
+  external: ["screeps-api", "git-rev-sync", "fs", "path"],
 };
